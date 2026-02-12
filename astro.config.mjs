4aa0781169b3a9a4350dc039439ab808e182dc3b
Choxy <choxypop@gmail.com>
2026-02-12 07:23:20 +0000

    Fix: force winston and logger inclusion in SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
-import vercel from '@astrojs/vercel'; // Cambiado de @astrojs/node
+import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
+import { defineConfig } from 'astro/config'
 export default defineConfig({
   output: 'server',
-  adapter: vercel(), // Cambiado para que Vercel sepa qué hacer
+  adapter: vercel(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
+    ssr: {
+      noExternal: ['@jsr/logan__logger', 'winston']
+    },
     resolve: {
       alias: {
         '@': new URL('./src', import.meta.url).pathname,
